7bbb12f36b590c4484f9e660f4acc22a6fda716c
Hector Flores <hecto932@gmail.com>
2016-04-11 23:54:48 -0430

    Fixed messages

--- application/modules/works/views/arbitration.php
+++ application/modules/works/views/arbitration.php
@@ -78,41 +78,8 @@
                 </div>
             </div>
 
-            <!-- 
-			<?php foreach($works as $key => $value): ?>
-
-				 <div class="col-lg-12">
-		            <div class="contact-box">
-		                <a href="backend/arbitraje/<?php echo $value["id"]; ?>">
-		                <div class="col-sm-3">
-		                    <div class="text-center">
-		                    <?php $image = "uploads/avatars/".$value['user']['image']; ?>
-		                    <?php if(file_exists($image) && $value['user']['image'] != null ): ?>
-		                    	<img alt="image" class="img-circle m-t-xs img-responsive" src="uploads/avatars/<?php echo $value["user"]["image"]; ?>">
-                            <?php else: ?>
-                                <img alt="image" class="img-circle m-t-xs img-responsive" src="assets/app/img/profile2.jpg">
-                            <?php endif; ?>
-		                    <div class="m-t-xs font-bold"><?php echo $value["user"]["university"]; ?></div>
-		                    </div>
-		                </div>
-		                <div class="col-sm-9">
-		                    <h3><strong><?php echo $value["user"]["lastName"].", ".$value["user"]["name"]; ?></strong></h3>
-		                    <p><i class="fa fa-map-marker"></i> <?php echo $value["campus"]; ?></p>
-		                    <address>
-		                        <strong><?php echo $value["title"]; ?></strong><br>
-		                        <strong><?php echo $value["area"]; ?></strong><br>
-		                        <a href="mailto:<?php echo $value["user"]["email"]; ?>"><?php echo $value["user"]["email"]; ?>"</a>
-		                    </address>
-		                </div>
-		                <div class="clearfix"></div>
-		                   </a>
-		            </div>
-		        </div>
-			<?php endforeach; ?>
-		<?php else: ?>
-			<p>No hay trabajos que arbitrar.</p>
-    	<?php endif; ?>
-
-    	-->
+       	<?php else: ?>
+       		<p>No hay trabajos que mostrar en esta categoria.</p>
+       	<?php endif; ?>
     </div>
 </div>
