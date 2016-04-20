b186ce27ee0db9bab7e7b1bfcd0268149af74360
Hector Flores <hecto932@gmail.com>
2016-04-20 14:27:51 -0430

    Cambios en la vista de pagos

--- application/modules/payments/views/payments.php
+++ application/modules/payments/views/payments.php
@@ -11,6 +11,7 @@
         </ol>
     </div>
 </div>
+
 <div class="row">
     <div class="col-lg-12">
         <div class="wrapper wrapper-content animated fadeInUp">
@@ -19,65 +20,65 @@
                 <div class="ibox-title">
                     <h5>Pagos</h5>
                     <div class="ibox-tools">
-                        <a href="backend/pagos/conforme"class="btn btn-primary dim" ><i class="fa fa-check"></i> Conforme</a>
-                        <a href="backend/pagos/no-conforme"class="btn btn-danger dim" ><i class="fa fa-warning"></i> No conforme</a>
+                        <a style="color:white;"href="backend/pagos"class="btn btn-info dim" ><i class="fa fa-check"></i> Por verificar</a>
+                        <a style="color:white;"href="backend/pagos/conforme"class="btn btn-primary dim" ><i class="fa fa-check"></i> Conforme</a>
+                        <a style="color:white;"href="backend/pagos/no-conforme"class="btn btn-danger dim" ><i class="fa fa-warning"></i> No conforme</a>
                     </div>
                 </div>
                 <div class="ibox-content">
+
                     <div class="project-list">
 
                         <table class="table table-hover">
                             <tbody>
-                                <?php if(!empty($payments)): ?>
-                                    <?php foreach($payments as $key => $value): ?>
-                                        <tr>
-                                            <td>
-                                                <?php echo $value["user"]; ?>
-                                            </td>
-                                            <td>
-                                                <?php echo $value["createdAt"]; ?>
-                                            </td>
-                                            <td>
-                                                #REF <strong><?php echo $value["numberReference"]; ?></strong>
-                                            </td>
-                                            <td>
-                                                <strong><?php echo $value["bank"]; ?></strong>
-                                            </td>
-                                            <td>
-                                                <strong>Bs. <?php echo $value["amount"]; ?></strong>
-                                            </td>
-                                            
-                                            <td>
-                                                <?php if($value["status"] == "Por verificar"): ?>
-                                                    <p><span style="font-size: 16px;"class="label label-primary"><?php echo $value["status"]; ?></span></p>
-                                                <?php else: ?>
-                                                    <p><span style="font-size: 16px;"class="label label-info"><?php echo $value["status"]; ?></span></p>
-                                                <?php endif; ?>
-                                            </td>
+                            <?php if(!empty($payments)): ?>
+                                <?php foreach($payments as $key => $value): ?>
+                                    <tr>
+                                        <td class="project-status">
                                             <?php if($value["status"] == "Por verificar"): ?>
-                                                <td >
-                                                    <form action="backend/pagos/verificar" method="post">
-                                                        <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash(); ?>">
-                                                        <input type="hidden" name="paymentId" value="<?php echo $value["id"]; ?>">
-                                                        <div class="form-group">
-                                                            <div class="col-sm-9">
-                                                                <select class="form-control m-b" name="status">
-                                                                    <option value="Por verificar"><?php echo $value["status"]; ?></option>
-                                                                    <option value="Conforme">Conforme</option>
-                                                                    <option value="No conforme">No conforme</option>
-                                                                </select>
-                                                            </div>
-                                                            <button class="col-sm-3 btn btn-sm btn-primary pull-right m-t-n-xs" type="submit"><strong>Aceptar</strong></button>
-                                                        </div>
-                                                    </form>
-                                                </td>
+                                                <span style="font-size: 14px;"class="label label-primary"><?php echo $value["status"]; ?></span>
+                                            <?php else: ?>
+                                                <span style="font-size: 14px;"class="label label-info"><?php echo $value["status"]; ?></span>
                                             <?php endif; ?>
-                                            
-                                        </tr>
-                                    <?php endforeach; ?>
-                                <?php else: ?>
-                                    <p>No hay existen pagos que mostrar en esta categoria.</p>
-                                <?php endif; ?>
+                                        </td>
+                                        <td class="project-title">
+                                            <a href="backend/participantes/<?php echo $value["user_id"]; ?>"><?php echo $value["user"]; ?></a>
+                                            <br>
+                                            <small>Realizado el <?php echo $value["createdAt"]; ?></small>
+                                        </td>
+                                        <td class="project-completion">
+                                                <small><?php echo $value["bank"]; ?></small><br>
+                                                <small>#REF <?php echo $value["numberReference"]; ?></small>
+                                        </td>
+                                        <td class="project-title">
+                                            <a href="javascript:void(0);">Bs. <?php echo $value["amount"]; ?></a>
+                                        </td>
+                                        <?php if($value["status"] == "Por verificar"): ?>
+                                            <td class="project-actions">
+                                                <form action="backend/pagos/verificar" method="post">
+                                                    <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash(); ?>">
+                                                    <input type="hidden" name="paymentId" value="<?php echo $value["id"]; ?>">
+                                                    <div class="form-group">
+                                                        <div class="col-sm-9">
+                                                            <select class="form-control m-b" name="status">
+                                                                <option value="Por verificar"><?php echo $value["status"]; ?></option>
+                                                                <option value="Conforme">Conforme</option>
+                                                                <option value="No conforme">No conforme</option>
+                                                            </select>
+                                                        </div>
+                                                        <button class="col-sm-3 btn btn-sm btn-primary pull-right m-t-n-xs" type="submit"><strong>Aceptar</strong></button>
+                                                    </div>
+                                                </form>
+                                            </td>
+                                        <?php endif; ?>
+                                    </tr>
+                                <?php endforeach; ?>
+                            <?php else: ?>
+                                <tr>
+                                     <p>No hay existen pagos que mostrar en esta categoria.</p>
+                                </tr>
+                            <?php endif; ?>
+                            
                             </tbody>
                         </table>
                     </div>
